b6873547de47388ac3e140b816611b1bfd565ac8
hoang1341995 <hoang1341995@gmail.com>
2022-07-15 14:09:26 +0700

    update
    update

--- ss2_typescript/exercise/fibonacci/src/fibonacci.js
+++ ss2_typescript/exercise/fibonacci/src/fibonacci.js
@@ -2,11 +2,13 @@ function fibonacci(count) {
     var total = 0;
     var number1 = 0;
     var number2 = 1;
+    var result = 0;
     for (var i = 1; i <= count; i++) {
         total = number1 + number2;
         number1 = number2;
         number2 = total;
+        result += total;
     }
-    return total;
+    return result;
 }
 console.log(fibonacci(10));
--- ss2_typescript/exercise/fibonacci/src/fibonacci.ts
+++ ss2_typescript/exercise/fibonacci/src/fibonacci.ts
@@ -3,12 +3,14 @@ function fibonacci(count : number) {
     let total : number = 0;
     let number1 : number = 0;
     let number2 : number = 1;
+    let result : number = 0;
     for (let i = 1; i <= count; i++) {
         total = number1 + number2;
         number1 = number2;
         number2 = total;
+        result += total;
     }
-    return total
+    return result;
 }
 
 console.log(fibonacci(10));
