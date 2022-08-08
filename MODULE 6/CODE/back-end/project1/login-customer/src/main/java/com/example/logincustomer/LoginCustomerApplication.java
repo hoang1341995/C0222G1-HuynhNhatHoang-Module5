d746435887ac73666f61d1f738de3fb88e999c73
hoang1341995 <hoang1341995@gmail.com>
2022-08-08 17:12:48 +0700

    UPDATE
    UPDATE

---
+++ MODULE 6/CODE/back-end/project1/login-customer/src/main/java/com/example/logincustomer/LoginCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.example.logincustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginCustomerApplication.class, args);
+    }
+
+}
